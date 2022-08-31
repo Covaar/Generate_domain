7ede53d93299a5fdbf497edb216cd34d030b7a48
Covaar <co.va.ard@gmail.com>
2022-08-31 23:29:10 +0000

    Generador_dominios

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import "bootstrap";
+/* import "bootstrap"; */
+
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
@@ -7,5 +8,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateDomain();
+  });
+  let generateDomain = () => {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let domain = [".com", ".us", ".net", ".cl"];
+    let dominios = [];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < domain.length; l++)
+            dominios.push(pronoun[i] + adj[j] + noun[k] + domain[l] + "\n");
+        }
+      }
+    }
+
+    return dominios;
+  };
+  console.log(generateDomain());
 };
